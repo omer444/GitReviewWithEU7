ec38e9d6c1089a5e9a58949f1e1db266da99e575
omer444 <ommerfar4@gmail.com>
2021-12-26 18:44:00 +0000

    feature a work

---
+++ src/test/java/day03/day3_work.java
@@ -0,0 +1,7 @@
+package day03;
+
+public class day3_work {
+    public static void main(String[] args) {
+        System.out.println("asdaskd");
+    }
+}
---
+++ src/test/java/featureA_work/featureAwork1.java
@@ -0,0 +1,9 @@
+package featureA_work;
+
+public class featureAwork1 {
+
+    public static void main(String[] args) {
+
+        System.out.println("feature a work");
+    }
+}
